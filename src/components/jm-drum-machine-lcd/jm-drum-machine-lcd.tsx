f088288c05987e85278058282f11d45459a944c1
Josh Mabry <mabry1985@gmail.com>
2021-02-12 09:50:44 -0800

    add listener for pad hover in lcd

--- src/components/jm-drum-machine-lcd/jm-drum-machine-lcd.tsx
+++ src/components/jm-drum-machine-lcd/jm-drum-machine-lcd.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import { Component, Host, h, Prop, Listen } from '@stencil/core';
 
 @Component({
   tag: 'jm-drum-machine-lcd',
@@ -8,12 +8,19 @@ import { Component, Host, h, Prop } from '@stencil/core';
 export class JmDrumMachineLcd {
   @Prop() AudioFileName: string;
 
+  @Listen('jmPadHovered', { target: "body" })
+  onDrumPadHovered(event: CustomEvent) {
+    if (event.detail && event.detail !== this.AudioFileName) {
+      this.AudioFileName = event.detail;
+    }
+  }
+
   render() {
     return (
       <Host>
         <div class="lcd-container">
           <h2 class="header">Drum Machine</h2>
-          <p class="audio-file-name">test{this.AudioFileName}</p>
+          <p class="audio-file-name">{this.AudioFileName}</p>
         </div>
       </Host>
     );
